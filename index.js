979be4cb2092fa7b33533a02febad471ba82c592
zachd93 <zachdebolt1993@gmail.com>
2024-03-01 14:52:47 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Step 1: Make sure that when a user visits the home page,
+//   it shows a random activity.You will need to check the format of the
+//   JSON data from response.data and edit the index.ejs file accordingly.
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&${req.body.participants}`);
+    const result = response.data;
+    const randomIndex = Math.floor(Math.random() * result.length);
+    res.render("index.ejs", { data: result, index: randomIndex });
+  } catch (error) {
+    console.error("Failed to make request: ", error.message);
+    res.render("index.ejs", { error: error.message });
+  }
+  // Step 3: If you get a 404 error (resource not found) from the API request.
+  // Pass an error to the index.ejs to tell the user:
+  // "No activities that match your criteria."
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
